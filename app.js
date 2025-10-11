0459494e9dbbd757ff335e4a309c557835eec7e3
Kiran-Ghosh004 <kiranghosh520@gmail.com>
2025-10-12 01:57:02 +0530

    learning to create rest api

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+const products_routes =require('./routes/products');
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.use('/api/products', require('./routes/products'));
+
+
+
+const start = async()=>{
+    try{
+        app.listen(PORT, () => {
+        console.log(`Example app listening at http://localhost:${PORT}`);
+    });
+    }catch(err){
+        console.error('Error starting server:', err);
+    }
+
+
+    
+}
+
+start();
+
