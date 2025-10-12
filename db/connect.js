22519e0dd475d144a7f4774dbd85751f2dfc0b2b
Kiran-Ghosh004 <kiranghosh520@gmail.com>
2025-10-12 13:50:11 +0530

    backend connected to db

--- db/connect.js
+++ db/connect.js
@@ -1,14 +1,12 @@
 const mongoose = require('mongoose');
 
-uri="mongodb+srv://kiranghosh520_db_user:Kiran@2003@kronapi.b8k2pr4.mongodb.net/kronAPI?retryWrites=true&w=majority&appName=kronAPI"
+// NOTE: I'm replacing 'Kiran@2003' with 'Kiran%402003'
+const uri = "mongodb+srv://kiranghosh520_db_user:Kiran%402003@kronapi.b8k2pr4.mongodb.net/kronAPI?retryWrites=true&w=majority&appName=kronAPI";
 
 const connectDB = async () => {
     console.log("connect db");
     
-    return mongoose.connect(uri,{
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    });
+    return mongoose.connect(uri)
 };
 
 module.exports = connectDB;
