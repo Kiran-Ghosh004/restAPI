2ce892eb9170600a42018cc7505660857bd3d342
Kiran-Ghosh004 <kiranghosh520@gmail.com>
2025-10-17 23:42:28 +0530

    pagination done

--- controllers/products.js
+++ controllers/products.js
@@ -27,6 +27,11 @@ const getAllProducts = async (req, res) => {
       result = result.sort(sortList);
     }
 
+    let page=Number(req.query.page) || 1;
+    let limit=Number(req.query.limit )|| 10;
+    let skip=(page-1)*limit;
+    result=result.skip(skip).limit(limit);
+
     const productsData = await result;
 
     res.status(200).json({ productsData, length: productsData.length });
@@ -59,6 +64,10 @@ const getAllProductsTesting = async (req, res) => {
       const sortList = sort.split(',').join(' ');
       result = result.sort(sortList);
     }
+    let page=Number(req.query.page) || 1;
+    let limit=Number(req.query.limit )|| 10;
+    let skip=(page-1)*limit;
+    result=result.skip(skip).limit(limit);
 
     const productsData = await result;
 
