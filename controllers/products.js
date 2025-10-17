5db4d80e25fce8ef24520dc112adedc54546543d
Kiran-Ghosh004 <kiranghosh520@gmail.com>
2025-10-17 22:12:32 +0530

    learnt about req.querry

--- controllers/products.js
+++ controllers/products.js
@@ -1,9 +1,20 @@
+
+const Product = require('../models/Product');
+
 const getAllProducts = async(req, res) => {
- res.status(200).json({msg: "get all products"});
+    
+    const productsData = await Product.find(req.query);
+    res.status(200).json({productsData,Length: productsData.length});
 }
 const getAllProductsTesting = async(req, res) => {
- res.status(200).json({msg: "get all products testing"});
+    const productsData = await Product.find(req.query);
+    res.status(200).json({
+        products: productsData,
+        length: productsData.length,
+        msg: "get all products testing"
+    });
 }
+
 
 
 module.exports = {getAllProducts, getAllProductsTesting};
